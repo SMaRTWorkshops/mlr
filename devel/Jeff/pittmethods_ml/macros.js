fda017ac967f854be96466fd545ea61e07210831
jmgirard <me@jmgirard.com>
2021-06-27 12:30:30 -0500

    Fix bug in emphasize macro

--- devel/Jeff/pittmethods_ml/macros.js
+++ devel/Jeff/pittmethods_ml/macros.js
@@ -1,4 +1,4 @@
 remark.macros.emphasize = function () {
   var text = this;
-  return '<span style="font-weight:bold;color:darkred;">' + text + '<span/>';
+  return '<span style="font-weight:bold;color:darkred;">' + text + '</span>';
 };
