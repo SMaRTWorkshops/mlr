5a71351c095f4ca09a81d476eae008d0a61d9be4
jmgirard <me@jmgirard.com>
2022-08-01 14:16:26 -0500

    Consolidate CSS

---
+++ xaringanthemer.R
@@ -0,0 +1,10 @@
+# Run to generate ./xaringan-themer.css
+# Include this css in the header of each xaringan file
+
+library(xaringanthemer)
+style_mono_light(
+  base_color = "#23395b",
+  footnote_font_size = "20px",
+  footnote_color = "gray",
+  text_slide_number_font_size = "18px"
+)
